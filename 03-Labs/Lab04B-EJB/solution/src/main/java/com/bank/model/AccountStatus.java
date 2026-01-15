3b74ca22cedec234e90f4795eb60c539f93a84df
Olivier Planson <olivier.planson@fr.ibm.com>
2026-01-15 17:49:09 +0100

    erreur de compilation lié à la visibilité de AccountStatus

---
+++ 03-Labs/Lab04B-EJB/solution/src/main/java/com/bank/model/AccountStatus.java
@@ -0,0 +1,14 @@
+package com.bank.model;
+
+/* © Copyright 2026 Olivier Planson. All rights reserved. Reproduction prohibited. Made with IBM Bob. */
+
+/**
+ * Account status enumeration.
+ */
+public enum AccountStatus {
+    ACTIVE,
+    SUSPENDED,
+    CLOSED
+}
+
+// Made with Bob
